dc6f94428192319c0ed61fec2510fc8794684abd
Yassine Benabbou <benabbou.yassine@yahoo.fr>
2018-03-19 21:12:17 +0000

    Edit init autoloader

--- app/init.php
+++ app/init.php
@@ -17,19 +17,6 @@ require_once __DIR__ . '/core/Autoloader.php';
 
 Core\Autoloader::register();
 
-/*
-spl_autoload_register(function ($class) {
-	$directory = NULL;
-	if (file_exists(__DIR__ . "/core/{$class}.php")) $directory = '/core/';
-	elseif(file_exists(__DIR__ . "/controllers/{$class}.php")) $directory = '/controllers/';
-	elseif(file_exists(__DIR__ . "/models/{$class}.php")) $directory = '/models/';
-	elseif(file_exists(__DIR__ . "/core/dao/{$class}.php")) $directory = '/core/dao/';
-    
-    if($directory) require_once __DIR__ . $directory . $class . '.php';
-});
-
-*/
-
 Dao\Database::connect();
 Models\Model::staticInit(Dao\DaoImpl::getInstance());
 Services\Session::start();
